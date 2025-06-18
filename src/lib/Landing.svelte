2f81149f4c8537d41d542575efa0ba57dc418088
Kellen <kellen.gary@gmail.com>
2025-06-18 12:28:18 -0400

    feat: add music showcase and update landing page with headshot link

--- src/lib/Landing.svelte
+++ src/lib/Landing.svelte
@@ -1,3 +1,6 @@
+<script>
+</script>
+
 <img src="/landing/m1.png"
      class="absolute bottom-20 w-auto max-h-[20vh] max-w-[20vw] object-contain transform scale-[0.85] sm:scale-90 md:scale-100"
      alt="m1"
@@ -20,10 +23,12 @@
 
 <div class="flex flex-col justify-center items-center gap-8 min-h-screen min-w-screen p-4 grow">
     <div class="flex flex-col md:flex-row gap-8 items-center">
-        <img src="/landing/headshot.jpeg"
-             class="rounded-full h-36 md:h-[20dvh]"
-             alt="headshot"
-        />
+        <a href="/secret">
+            <img src="/landing/headshot.jpeg"
+                 class="rounded-full h-36 md:h-[20dvh]"
+                 alt="headshot"
+            />
+        </a>
         <div class="flex flex-col  gap-4">
             <p class="font-bold text-2xl leading-0 md:text-5xl sm:text-4xl md:leading-[0.7]">Student at <span class="underline decoration-accent-red underline-offset-6 ">The Ohio State University</span></p>
             <p class="text-xl md:text-4xl sm:text-3xl">Studying Computer and Information Science</p>
---
+++ src/routes/secret/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+<main class="flex flex-col items-center justify-center p-4 h-screen">
+    <div class="relative flex flex-row items-center w-full h-fit">
+        <a href="/" class="absolute" aria-label="home button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:stroke-3 hover:animate-pulse size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            </svg>
+        </a>
+        <h1 class="text-center flex-1 font-bold">Music</h1>
+    </div>
+
+    <div class="flex-1 flex flex-col gap-8 justify-center">
+        <a href="https://untitled.stream/library/project/vf0xTzi9b7ePPhz4Ptw2b" class="flex flex-col gap-2">
+            <img src="/altruist.jpeg" alt="altruist" class="size-48 rounded-lg"/>
+            <div class="flex flex-row justify-between pr-1 items-center">
+                <p class="font-semibold leading-4 text-sm">Altruist</p>
+                <p class="text-sm opacity-50 leading-4">6 tracks</p>
+            </div>
+        </a>
+        <a href="https://untitled.stream/library/project/Wqs07Qc408uV3SaAnSrEw" class="flex flex-col gap-2">
+            <img src="/singles.jpeg" alt="singles" class="size-48 rounded-lg"/>
+            <div class="flex flex-row justify-between pr-1 items-center">
+                <p class="font-semibold leading-4 text-sm">Singles</p>
+                <p class="text-sm opacity-50 leading-4">10 tracks</p>
+            </div>
+        </a>
+        <a href="https://untitled.stream/library/project/o0cW2t24JzKQLsgei9qQp" class="flex flex-col gap-2">
+            <img src="/youve_never.jpeg" alt="you've never" class="size-48 rounded-lg"/>
+            <div class="flex flex-row justify-between pr-1 items-center">
+                <p class="font-semibold leading-4 text-sm">You've Never</p>
+                <p class="text-sm opacity-50 leading-4">8 tracks</p>
+            </div>
+        </a>
+    </div>
+
+</main>
